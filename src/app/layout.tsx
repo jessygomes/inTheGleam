853eb18ede39542ead6846dc49e1cd31e2cf3e7b
Jessy <you@example.com>
2025-01-29 12:14:38 +0100

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,8 +61,26 @@ export default function RootLayout({
             ],
           })}
         </script>
+
+        <script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5JM23FR7');
+        `}
+        </script>
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JM23FR7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Analytics />
         <div className="fixed top-0 z-50 w-full flex justify-center">
           <Navbar />
